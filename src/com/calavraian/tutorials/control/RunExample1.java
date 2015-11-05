68e5e0372d38760f73a1a1173cbd8596c4aa1136
Juan Carlos Araujo Borrego <jcborre@digono.com>
2015-11-05 17:24:10 -0600

    Comments added to explain the process on each case

--- src/com/calavraian/tutorials/control/RunExample1.java
+++ src/com/calavraian/tutorials/control/RunExample1.java
@@ -12,7 +12,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class RunExample1 {
 	
 	public static void main(String[] args) {
-		//Example 1
+		//Ejemplo 1
 		readJSON();
 		
 		writeJSON();
@@ -20,12 +20,15 @@ public class RunExample1 {
 
 	private static void writeJSON() {
 		People objectToParse = new People();
-		ObjectMapper mapperObj = new ObjectMapper();
+		ObjectMapper mapperObj = new ObjectMapper(); // Se construye una sola vez y se puede utilizar en todos los casos presentados abajo
 
+		// Se registran los valores de la instancia del objeto que sera usado en todos los casos siguientes
 		objectToParse.name = "Comming Soon";
 		objectToParse.email = "comming@example.com";
 		objectToParse.age = 0;
 		
+		// El try catch es necesario para este tipo de evaluacion pero se puede hacer un through al inicio de la clase
+		// por otro lado se puede hacer un mejor detalle del catch para personalizar las excepciones posibles y no usar una sola generica
 		try {
 			mapperObj.writeValue(new File("Resources/Example1Output.json"), objectToParse);
 			
@@ -34,7 +37,8 @@ public class RunExample1 {
 			e.printStackTrace();
 		}
 		
-		// or:
+		// otra forma:
+		// Los bytes se mostraran como un arreglo de enteros, si se necesita el caracter es necesario convertirlo
 		try {
 			byte[] jsonBytes = mapperObj.writeValueAsBytes(objectToParse);
 			
@@ -43,7 +47,8 @@ public class RunExample1 {
 			e.printStackTrace();
 		}
 
-		// or:
+		// otra forma:
+		// Se obtiene el valor completo del objeto en una cadena en formato de JSON
 		try {
 			String jsonString = mapperObj.writeValueAsString(objectToParse);
 			
@@ -54,8 +59,12 @@ public class RunExample1 {
 	}
 
 	private static void readJSON() {
-		ObjectMapper mapperObj = new ObjectMapper();
+		ObjectMapper mapperObj = new ObjectMapper(); // Se construye una sola vez y se puede utilizar en todos los casos presentados abajo
 
+		
+		// Lectura directa de un archivo dentro del proyecto
+		// El try catch es necesario para este tipo de evaluacion pero se puede hacer un through al inicio de la clase
+		// por otro lado se puede hacer un mejor detalle del catch para personalizar las excepciones posibles y no usar una sola generica
 		try {
 			People dataFromFile = mapperObj.readValue(new File("Resources/DataExample1.json"), People.class);
 			
@@ -66,7 +75,8 @@ public class RunExample1 {
 			e.printStackTrace();
 		}
 		
-		// or:
+		// otra forma:
+		// Directo de un archivo en un servidor (URL)
 		try {
 			People dataFromURL = mapperObj.readValue(new URL("http://calavraian.com/DataExample1.json"), People.class);
 			
@@ -77,7 +87,10 @@ public class RunExample1 {
 			e.printStackTrace();
 		}
 
-		// or:
+		// otra forma:
+		// Escribiendo la cadena directo en el metodo, recuerden espacar el caracter "
+		// Otra forma es obviamente creando una cadena tipo String tomando valores de diferentes lados y despues se para
+		// como parametro
 		try {
 			People dataFromText = mapperObj.readValue("{\"name\":\"Elizabeth\", \"email\":\"eliza@example.com\", \"age\":25}", People.class);
 			
